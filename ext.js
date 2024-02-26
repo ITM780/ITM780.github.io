114997403dba3641c9a125c0bff02e77a31ae424
ITM780 <157924180+ITM780@users.noreply.github.com>
2024-02-26 11:44:42 -0500

    Update ext.js

--- ext.js
+++ ext.js
@@ -5,11 +5,11 @@ function promptName() {
 }
 function enlargeImage() {
   var image = document.getElementById("Image") ;
-  image.style.width = 150px ;
+  image.style.width = 60px ;
 }
 function resetImageSize() {
   var image = document.getElementById("Image");
-  image.style.width = 100px ;
+  image.style.width = 40px ;
 }
 function mouseover(element) {
   element.style.backgroundColor = #325 ;
