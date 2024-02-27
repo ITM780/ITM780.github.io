fe7b3d77768adb2abcf248bc4011dea562aef460
ITM780 <157924180+ITM780@users.noreply.github.com>
2024-02-26 19:20:52 -0500

    Update ext.js

--- ext.js
+++ ext.js
@@ -7,10 +7,13 @@ function enlargeImage(){
     image.style.width = (image.clientWidth + 250) + "px";
     image.style.height = (image.clientHeight + 200) + "px";
 }
-function mouseover(element) {
-  element.style.backgroundColor = #C0C0C0;
+function mouseOver(element){
+    element.style.color = "#ADD8E6";
+    element.style.fontWeight = "bold";
+    element.style.fontSize = "18px";
 }
-function mouseout(element) {
-    element.style.backgroundColor = 333;
+function mouseOut(element){
+    element.style.color = "#000000";
+    element.style.fontWeight = "normal";
+    element.style.fontSize = "inherit"; 
 }
-
