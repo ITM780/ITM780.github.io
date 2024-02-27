07e936075682560b169c514164bacdf8c713eda8
ITM780 <157924180+ITM780@users.noreply.github.com>
2024-02-26 20:04:42 -0500

    Update ext.js

--- ext.js
+++ ext.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var nameButton = document.getElementById('nameButton');
+    var nameB = document.getElementById('nameB');
     var outputElement = document.getElementById('output');
 
-    nameButton.addEventListener('click', function() {
+    nameB.addEventListener('click', function() {
         var name = prompt('Please enter your name:');
         if (name) {
             outputElement.textContent = 'Hello, ' + name + '!';
